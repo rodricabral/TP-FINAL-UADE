9c5361cac95a09c87dcb40bdd87205e7a6aa011f
AgustinR39 <agustinronchi15@gmail.com>
2024-12-11 02:11:00 -0300

    Cambios en proveedor

--- Frontend/src/componentes/Proveedor.js
+++ Frontend/src/componentes/Proveedor.js
@@ -1,115 +1,97 @@
-
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../componentes/estilos.css"; 
+import "../componentes/estilos.css";
+import { Dialog } from "primereact/dialog";
+import { Button } from "primereact/button";
 
 function Proveedor() {
   const [id, setId] = useState("");
   const [nombre, setNombre] = useState("");
   const [cuit, setCuit] = useState("");
-  const [editMode, setEditMode] = useState(false);
-  const [editingIndex, setEditingIndex] = useState(null);
-  const [proveedorList, setProveedorList] = useState([]); 
 
- 
-  const fetchProveedorList = async () => {
-    try {
-      const response = await fetch("http://localhost:3002/api/proveedor/el-proveedor");
-      const data = await response.json();
-      setProveedorList(data); 
-    } catch (error) {
-      console.error("Error al cargar los proveedores", error);
-    }
-  };
+  const [proveedorList, setProveedorList] = useState([]);
+  const [visible, setVisible] = useState(false);
 
-  
+  // Lista proveedores
   useEffect(() => {
+    const fetchProveedorList = async () => {
+      try {
+        const response = await axios.get("http://localhost:3002/api/proveedor/el-proveedor");
+        setProveedorList(response.data);
+      } catch (error) {
+        console.error("Error al cargar los proveedores", error);
+      }
+    };
     fetchProveedorList();
   }, []);
 
-  
-  const handleSubmit = async (e) => {
+  // Añadir proveedor
+  const agregarProveedor = async (e) => {
     e.preventDefault();
-
-    
-    if (!nombre || !cuit) {
-      alert("Por favor, complete ambos campos: nombre y cuit.");
-      return; 
+    try {
+      await axios.post("http://localhost:3002/api/proveedor/guardar", { nombre, cuit });
+      alert("Proveedor guardado con éxito");
+      actualizarLista();
+      limpiarCampos();
+    } catch (error) {
+      console.error("Error al guardar el proveedor", error);
+      alert("Hubo un error al guardar el proveedor");
     }
-
-    
-    const formData = { nombre, cuit };
-
-    if (editMode) {
-      
-      const updatedList = proveedorList.map((item, index) =>
-        index === editingIndex ? { ...item, nombre, cuit } : item
-      );
-      setProveedorList(updatedList);
-      setEditMode(false);
-      setEditingIndex(null);
-    } else {
-      
-      try {
-        await axios.post("http://localhost:3002/api/proveedor/guardar", formData, {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
-        alert("Proveedor guardado con éxito");
-        fetchProveedorList(); 
-      } catch (error) {
-        console.error("Error al guardar el proveedor", error);
-        alert("Hubo un error al guardar el proveedor");
-      }
-    }
-
-    limpiarCampos();
   };
 
-  
-  const handleEdit = (proveedor) => {
-    setId(proveedor.id); 
-    setNombre(proveedor.nombre);
-    setCuit(proveedor.cuit);
-    setEditMode(true); 
-    setEditingIndex(proveedor.id); 
-  };
-
-  
-  const limpiarCampos = () => {
-    setId("");
-    setNombre("");
-    setCuit("");
-    setEditMode(false);
-  };
-
-  
-  const updateProveedor = async () => {
+  // Edicion
+  const editarProveedor = async () => {
     try {
       await axios.put(`http://localhost:3002/api/proveedor/modificar-proveedor/${id}`, {
-        id: id,
-        nombre: nombre,
-        cuit: cuit,
+        id,
+        nombre,
+        cuit,
       });
-      alert("Datos actualizados con éxito");
-      fetchProveedorList(); 
+      alert("Proveedor actualizado con éxito");
+      actualizarLista();
+      setVisible(false);
       limpiarCampos();
     } catch (error) {
       console.error("Error al actualizar el proveedor", error);
     }
   };
 
-  const eliminarProveedor = async (id) => {
+  // Eliminar
+  const eliminarProveedor = async () => {
     try {
       await axios.delete(`http://localhost:3002/api/proveedor/eliminar/${id}`);
-      setProveedorList(proveedorList.filter((proveedor) => proveedor.id !== id));
-      fetchProveedorList(); 
       alert("Proveedor eliminado con éxito");
+      setProveedorList(proveedorList.filter((proveedor) => proveedor.id !== id));
+      setVisible(false);
+      limpiarCampos();
     } catch (error) {
       console.error("Error al eliminar el proveedor", error);
       alert("Hubo un error al eliminar el proveedor");
     }
+  };
+
+  // Actualizar
+  const actualizarLista = async () => {
+    try {
+      const response = await axios.get("http://localhost:3002/api/proveedor/el-proveedor");
+      setProveedorList(response.data);
+    } catch (error) {
+      console.error("Error al actualizar la lista de proveedores", error);
+    }
+  };
+
+  const handleEdit = (proveedor) => {
+    setId(proveedor.id);
+    setNombre(proveedor.nombre);
+    setCuit(proveedor.cuit);
+    setVisible(true);
+  };
+
+  // Limpiar campos
+  const limpiarCampos = () => {
+    setId("");
+    setNombre("");
+    setCuit("");
   };
 
   return (
@@ -119,53 +101,23 @@ function Proveedor() {
           <h2 className="text-center bg-dark p-2 text-warning">Datos de los Proveedores</h2>
         </div>
         <div className="card-body">
-          <form onSubmit={handleSubmit}>
+          {/* Agregar proveedor */}
+          <form onSubmit={agregarProveedor}>
             <div className="input-group mb-3 bg-dark p-2 text-white bg-opacity-75">
-              <label className="input-group-text bg-dark p-2 text-white bg-opacity-75" htmlFor="nombre">
-                Nombre
-              </label>
-              <input
-                className="form-control"
-                id="nombre"
-                type="text"
-                name="nombre"
-                placeholder="Ingrese el nombre del Proveedor"
-                value={nombre}
+              <label className="input-group-text" htmlFor="nombre">Nombre</label>
+              <input className="form-control" id="nombre" type="text" value={nombre}
                 onChange={(e) => setNombre(e.target.value)}
                 required
               />
             </div>
             <div className="input-group mb-3 bg-dark p-2 text-white bg-opacity-75">
-              <label className="input-group-text bg-dark p-2 text-white bg-opacity-75" htmlFor="cuit">
-                Cuit
-              </label>
-              <input
-                className="form-control"
-                id="cuit"
-                type="text"
-                name="cuit"
-                placeholder="Ingrese el cuit del Proveedor"
-                value={cuit}
+              <label className="input-group-text" htmlFor="cuit">Cuit</label>
+              <input className="form-control" id="cuit" type="text" value={cuit}
                 onChange={(e) => setCuit(e.target.value)}
                 required
               />
             </div>
-            <div className="card-footer text-body-secondary">
-              {editMode ? (
-                <div>
-                  <button className="btn btn-dark m-2" onClick={updateProveedor}>
-                    Actualizar
-                  </button>
-                  <button className="btn btn-danger m-2" onClick={limpiarCampos}>
-                    Cancelar
-                  </button>
-                </div>
-              ) : (
-                <button className="btn btn-warning float-end" type="submit">
-                  <b>Agregar</b>
-                </button>
-              )}
-            </div>
+            <button className="btn btn-warning float-end" type="submit"> <b>Agregar Proveedor</b> </button>
           </form>
         </div>
       </div>
@@ -175,10 +127,10 @@ function Proveedor() {
         <table className="table table-striped" style={{ minWidth: "50rem" }}>
           <thead>
             <tr>
-              <th scope="col">Id</th>
-              <th scope="col">Nombre</th>
-              <th scope="col">Cuit</th>
-              <th scope="col">Acciones</th>
+              <th>Id</th>
+              <th>Nombre</th>
+              <th>Cuit</th>
+              <th>Acciones</th>
             </tr>
           </thead>
           <tbody>
@@ -188,30 +140,41 @@ function Proveedor() {
                 <td>{proveedor.nombre}</td>
                 <td>{proveedor.cuit}</td>
                 <td>
-                  <div className="btn-group" role="group" aria-label="Basic example">
-                    <button
-                      type="button"
-                      className="btn btn-warning m-2"
-                      onClick={() => handleEdit(proveedor)}
-                    >
-                      Editar
-                    </button>
-                    <button
-                      type="button"
-                      className="btn btn-dark m-2"
-                      onClick={() => eliminarProveedor(proveedor.id)}
-                    >
-                      Eliminar
-                    </button>
-                  </div>
+                  <Button label="Ver" icon="pi pi-pencil" onClick={() => handleEdit(proveedor)} className="p-button-warning" />
                 </td>
               </tr>
             ))}
           </tbody>
         </table>
       </div>
+
+      {/* Editar, eliminar */}
+      <Dialog header="Editar Proveedor" visible={visible} style={{ width: "50vw" }}
+        onHide={() => setVisible(false)}
+      >
+        <form>
+          <div className="input-group mb-3">
+            <span className="input-group-text">Nombre</span>
+            <input type="text" className="form-control" value={nombre}
+              onChange={(e) => setNombre(e.target.value)}
+              required
+            />
+          </div>
+          <div className="input-group mb-3">
+            <span className="input-group-text">Cuit</span>
+            <input type="text" className="form-control" value={cuit}
+              onChange={(e) => setCuit(e.target.value)}
+              required
+            />
+          </div>
+          <div className="d-flex justify-content-between">
+            <Button label="Actualizar" icon="pi pi-check" onClick={editarProveedor} className="p-button-success" />
+            <Button label="Eliminar" icon="pi pi-trash" onClick={eliminarProveedor} className="p-button-danger" />
+          </div>
+        </form>
+      </Dialog>
     </div>
   );
 }
 
-export default Proveedor;
+export default Proveedor;
